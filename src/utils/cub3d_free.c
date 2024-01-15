d445b0e2563b9774775880f29e3790a92e2b0da2
Eli EWU <eewu@paul-f4Br3s2.clusters.42paris.fr>
2024-01-15 13:31:55 +0100

    delete

--- src/utils/cub3d_free.c
+++ src/utils/cub3d_free.c
@@ -6,7 +6,7 @@
 /*   By: eewu <eewu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 13:19:14 by eewu              #+#    #+#             */
-/*   Updated: 2024/01/15 13:21:36 by eewu             ###   ########.fr       */
+/*   Updated: 2024/01/15 13:24:57 by eewu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 void	ft_free_split(char **split)
 {
-	(void)split;
+	int	i;
+
+	i = 0;
+	if (!split)
+		return (0);
+	while (split[i])
+		free(split[i++]);
+	free(split);
+	return (0);
 }
