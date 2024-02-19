f8fd5b5cdb24d8d0bf37e4171fa3e93c2825792c
Ombeline DE CLEBSATTEL <ode-cleb@bess-f1r5s11.clusters.42paris.fr>
2024-02-19 17:09:58 +0100

    souris

--- src/utils/cub3d.c
+++ src/utils/cub3d.c
@@ -12,9 +12,29 @@
 
 #include "../../include/Cub3d.h"
 
+int	ft_mouse_event(int x, int y, t_vars *vars)
+{
+	t_ray		r;
+	double		planx_tmp;
+	double		dirx_tmp;
+	static int	prev_x;
+
+	(void)y;
+	r = vars->ray;
+	planx_tmp = r.planx;
+	dirx_tmp = r.dirx;
+	double delta = (x - prev_x) * 0.003; // calcule delta souris
+	r.dirx = r.dirx * cos(delta) - r.diry * sin(delta);
+	r.diry = dirx_tmp * sin(delta) + r.diry * cos(delta);
+	r.planx = r.planx * cos(delta) - r.plany * sin(delta);
+	r.plany = planx_tmp * sin(delta) + r.plany * cos(delta);
+	vars->ray = r;
+	prev_x = x;
+	return (0);
+}
+
 void	ft_mlx_start(t_vars *vars)
 {
-
 	vars->mlx = mlx_init();
 	if (vars->mlx == NULL)
 		ft_free_mlx(1, vars);
@@ -23,6 +43,7 @@ void	ft_mlx_start(t_vars *vars)
 	mlx_hook(vars->win, 2, 1L << 0, ft_keycode, vars);
 	mlx_loop_hook(vars->mlx, &ft_display, vars);
 	mlx_hook(vars->win, 3, 1L << 1, ft_keycode_r, vars);
+	mlx_hook(vars->win, 06, 1L << 6, ft_mouse_event, vars);
 	mlx_hook(vars->win, 17, 1L << 2, mlx_loop_end, vars->mlx);
 	mlx_loop(vars->mlx);
 	ft_free_mlx(0, vars);
